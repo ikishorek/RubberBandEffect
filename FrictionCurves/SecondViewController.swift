95c23e40ed7e886f7619db557ce17ae29ef1ff2b
Kishore <ukisho@gmail.com>
2017-07-22 13:16:01 +0530

    updated swift version

--- FrictionCurves/SecondViewController.swift
+++ FrictionCurves/SecondViewController.swift
@@ -19,28 +19,28 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func viewDragged(sender: UIPanGestureRecognizer) {
-        let yTranslation = sender.translationInView(view).y
+    @IBAction func viewDragged(_ sender: UIPanGestureRecognizer) {
+        let yTranslation = sender.translation(in: view).y
         
         if (topViewConstraint.hasExceeded(verticalLimit)){
             totalTranslation += yTranslation
             topViewConstraint.constant = logConstraintValueForYPosition(totalTranslation)
-            if(sender.state == UIGestureRecognizerState.Ended ){
+            if(sender.state == UIGestureRecognizerState.ended ){
                 animateViewBackToLimit()
             }
         } else {
             topViewConstraint.constant += yTranslation
         }
-        sender.setTranslation(CGPointZero, inView: view)
+        sender.setTranslation(CGPoint.zero, in: view)
     }
     
-    func logConstraintValueForYPosition(yPosition : CGFloat) -> CGFloat {
+    func logConstraintValueForYPosition(_ yPosition : CGFloat) -> CGFloat {
         return verticalLimit * (1 + log10(yPosition/verticalLimit))
     }
     func animateViewBackToLimit() {
         self.topViewConstraint.constant = self.verticalLimit
         
-        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 10, options: UIViewAnimationOptions.AllowUserInteraction, animations: { () -> Void in
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 10, options: UIViewAnimationOptions.allowUserInteraction, animations: { () -> Void in
             self.view.layoutIfNeeded()
             self.totalTranslation = -200
             }, completion: nil)
@@ -48,7 +48,7 @@ class SecondViewController: UIViewController {
 }
 
 private extension NSLayoutConstraint {
-    func hasExceeded(verticalLimit: CGFloat) -> Bool {
+    func hasExceeded(_ verticalLimit: CGFloat) -> Bool {
         return self.constant < verticalLimit
     }
 }
